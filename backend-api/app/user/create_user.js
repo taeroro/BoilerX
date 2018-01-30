2862e3622a95dab8bed697fe848b9d651417169c
LING ZHANG <junelena1313@gmail.com>
2018-01-30 12:35:45 -0500

    configure serverless and api: able to work

---
+++ backend-api/app/user/create_user.js
@@ -0,0 +1,36 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../utils/dynamodb-lib";
+import { success, failure } from "../utils/response-lib";
+
+export async function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "User",
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      email: data.email,
+      pass: data.pass,
+      createdAt: new Date().getTime()
+    }
+
+  };
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    console.log("Got error:", err.message);
+    console.log("Request:");
+    console.log(this.request.httpRequest);
+    console.log("Response:");
+    console.log(this.httpResponse);
+    callback(null, failure({ status: false }));
+  }
+
+}
