c427d4371d8e43e1d761bb3018fa16e84b54f2aa
Ryan Fan <fanzuyuan1996@gmail.com>
2018-02-11 16:10:07 -0500

    Now can login successfully
    Only limited functions.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import fetchItem from './reducer_fetchItem';
 
 const rootReducer = combineReducers({
-  items: fetchItem
+  items: fetchItem,
+  form: formReducer
 });
 
 export default rootReducer;
