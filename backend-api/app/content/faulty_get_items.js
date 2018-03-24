51c1ebd424895508fe4780ecd767cda0717503b9
LING ZHANG <junelena1313@gmail.com>
2018-03-24 18:34:31 -0400

    faulty homepage

---
+++ backend-api/app/content/faulty_get_items.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+/**
+ * @api {get} /content Get top 5 items ranked by popularity for homepage.
+ * @apiName getItems
+ * @apiGroup content
+ * 
+ * @apiSuccess {Object[]}
+ * @apiSuccess {JSON} status false
+ */
+export async function main(event, context, callback) {
+  
+  const params = {
+    TableName: "Item"
+  };
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    result.Items.sort(function(a, b) {
+      return parseInt(a.popularity) - parseInt(b.popularity);
+    });
+    callback(null, success(result.Items));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
